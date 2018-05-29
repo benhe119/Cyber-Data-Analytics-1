e414fc95befca1e364a5cabff2a9cafa9aff06a1
Ernst Mulders <ernst.mulders@gmail.com>
2018-05-29 11:59:24 +0200

    Small changes in run file

--- lab2/processTrainingDataset1.py
+++ lab2/processTrainingDataset1.py
@@ -20,16 +20,17 @@ import scipy.fftpack as fftpk
 import scipy.signal as sgnl
 from filter_data import filter, filter_F_PUxx, filter_S_PUxx, filter_P_Jxxx
 from discretize_data import discretizeBinary, discretizeSAX
+from PCA import PCA, PCA_detection
 
 ### READ AND EDIT CSV FILE ###   ###############################################
-df = pd.read_csv("./data/BATADAL_training_dataset_1.csv",delimiter=',');
+df = pd.read_csv("./data/BATADAL_training_dataset_1.csv",delimiter=',',parse_dates=True, index_col='DATETIME');
 #,names=["DATETIME", "L_T1", "L_T2", "L_T3", "L_T4", "L_T5", "L_T6", "L_T7", "F_PU1", "S_PU1", "F_PU2", "S_PU2", "F_PU3", "S_PU3", "F_PU4", "S_PU4"])
 #, "F_PU5, "S_PU5", F_PU6, S_PU6, F_PU7, S_PU7, F_PU8, S_PU8, F_PU9, S_PU9, F_PU10, S_PU10, F_PU11, S_PU11, F_V2, S_V2, P_J280, P_J269, P_J300, P_J256,
 #P_J289, P_J415, P_J302, P_J306, P_J307, P_J317, P_J14, P_J422, ATT_FLAG])
 #print list(df.columns.values)
 #df.sort(columns='L_T1',axis=0,ascending=True,inplace=True)
 # dat_L_T1.plot(figsize=(6,4))
-df.sort_values(by = 'DATETIME', ascending = True, inplace=True)
+#df.sort_values(by = 'DATETIME', ascending = True, inplace=True)
 
 # print '\ndurbin watson statistics: ' + str(sm.stats.durbin_watson(dta))
 
@@ -40,7 +41,7 @@ S_PUxx = ["S_PU1", "S_PU2", "S_PU3", "S_PU4", "S_PU5", "S_PU6", "S_PU7", "S_PU8"
 P_Jxxx = ['P_J280', 'P_J269', 'P_J300', 'P_J256', 'P_J289', 'P_J415', 'P_J302', 'P_J306', 'P_J307', 'P_J317', 'P_J14', 'P_J422']
 Attack = ['ATT_FLAG']
 
-analysisMethod = 'N-gram'
+analysisMethod = 'PCA'
 
 if analysisMethod == 'ARMA':
     print 'arma'
@@ -52,7 +53,8 @@ elif analysisMethod == 'N-gram':
     discretizeSAX(L_Txx,df)
 
 elif analysisMethod == 'PCA':
-    print 'pca'
+	#PCA(df) #sub selection of data
+    PCA_detection(df)
 else:
     # do nothing
     print 'no analysis method'
