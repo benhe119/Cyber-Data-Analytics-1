8e31ffa3f8af6f8cd165c3a1e5e1fc68bc501dd5
Alex <berndtae@gmail.com>
2018-06-18 16:24:55 +0200

    Filter only other addresses which host connects with

--- lab3/readData.py
+++ lab3/readData.py
@@ -3,7 +3,11 @@ CDA Lab 3
 
 Process data of CTU13 data
 
-# download data from ...
+download data from https://mcfp.felk.cvut.cz/publicDatasets/CTU-Malware-Capture-Botnet-46/
+
+file: 'capture20110815-2.pcap.netflow.labeled'
+parsed using: parse_data.py
+
 """
 
 print 'Cyber Data Analytics\nLab 3 - Streaming Data\n'
@@ -13,43 +17,39 @@ from scipy import stats
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from pandas.tools.plotting import autocorrelation_plot
+# from pandas.tools.plotting import autocorrelation_plot
 from statsmodels.graphics.api import qqplot
-import scipy.fftpack as fftpk
-import scipy.signal as sgnl
+# import scipy.fftpack as fftpk
+# import scipy.signal as sgnl
 from matplotlib import pyplot as plt
 
-# from pcapfile import savefile
-# from pcapfile.protocols.linklayer import ethernet
-# from pcapfile.protocols.network import ip
-#
-# import sys
-# sys.path.insert(0, 'other/pcap2frame-master')
-# import pcap2frame
-#
-# import pyshark
-#
-# cap = pyshark.FileCapture('data/capture20110815-2.pcap.netflow.labeled')
+# Read data from CTU113 dataset
 
-df_ctu13_42 = pd.read_csv("./data/capture20110815-2.pcap.netflow.labeled.csv", delimiter=',', parse_dates=True, dayfirst=True) #, index_col='DATETIME')
+# dataset 42
+df_ctu13_42 = pd.read_csv("./data/capture20110815-2.pcap.netflow.labeled.csv", delimiter=',', parse_dates=True, dayfirst=True)#, index_col='Date flow start')
+
+# Describe Data
+# print df_ctu13_42.describe()
+# print df_ctu13_42.head()
+# print df_ctu13_42.columns.values
+# print df_ctu13_42.columns
+
+# ==============================================================================
+#                   Apply minhashing a.k.a min-wise hashing
+# ==============================================================================
+
+# we see that the host's IP address is 147.32.***.***
+HostIP = '147.32.'
+
+# only consider rows where the source (SRC) is the host IP
+df_ctu13_42 = df_ctu13_42[(df_ctu13_42["SrcIPAddr_Port"].str.contains(HostIP))]
 
 print df_ctu13_42.describe()
 
-print df_ctu13_42.head()
-
-print df_ctu13_42.columns.values
+# only consider fields where hostIP is associated with
 
 
-# testcap = open('data/capture20110815-2.pcap.netflow.labeled', 'rb')
-#
-# print testcap
 
-# capfile = savefile.load_savefile(testcap, verbose=True)
-#
-# eth_frame = ethernet.Ethernet(capfile.packets[0].raw())
-# print eth_frame
-#
-# ip_packet = ip.IP(binascii.unhexlify(eth_frame.payload))
-# print ip_packet
-
-# 'capture20110815-2.pcap.netflow.labeled'
+# ==============================================================================
+#                         Apply Min-Count hashing
+# ==============================================================================
